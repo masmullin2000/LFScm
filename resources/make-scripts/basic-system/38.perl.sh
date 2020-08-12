8707191cc3225b42e3f172a2ec5b7861a050e0c7
Michael Mullin <masmullin@gmail.com>
2020-08-11 23:22:07 -0400

    some cleanup and get us uptodate with 20200810

--- resources/make-scripts/basic-system/38.perl.sh
+++ resources/make-scripts/basic-system/38.perl.sh
@@ -11,9 +11,9 @@ export BUILD_BZIP2=0
 sh Configure -des                                         \
              -Dprefix=/usr                                \
              -Dvendorprefix=/usr                          \
-             -Dprivlib=/usr/share/perl5/core_perl         \
+             -Dprivlib=/usr/lib/perl5/5.32/core_perl      \
              -Darchlib=/usr/lib/perl5/5.32/core_perl      \
-             -Dsitelib=/usr/share/perl5/site_perl         \
+             -Dsitelib=/usr/lib/perl5/5.32/site_perl      \
              -Dsitearch=/usr/lib/perl5/5.32/site_perl     \
              -Dvendorlib=/usr/share/perl5/vendor_perl     \
              -Dvendorarch=/usr/lib/perl5/5.32/vendor_perl \
--- resources/make-scripts/toolchain/additional/25.perl.sh
+++ resources/make-scripts/toolchain/additional/25.perl.sh
@@ -5,14 +5,14 @@ set -e
 tar xvf perl.tar.gz
 cd perl
 
-sh Configure -des                                     \
-             -Dprefix=/usr                            \
-             -Dvendorprefix=/usr                      \
-             -Dprivlib=/usr/share/perl5/core_perl     \
-             -Darchlib=/usr/lib/perl5/5.32/core_perl  \
-             -Dsitelib=/usr/share/perl5/site_perl     \
-             -Dsitearch=/usr/lib/perl5/5.32/site_perl \
-             -Dvendorlib=/usr/share/perl5/vendor_perl \
+sh Configure -des                                        \
+             -Dprefix=/usr                               \
+             -Dvendorprefix=/usr                         \
+             -Dprivlib=/usr/lib/perl5/5.32/core_perl     \
+             -Darchlib=/usr/lib/perl5/5.32/core_perl     \
+             -Dsitelib=/usr/lib/perl5/5.32/site_perl     \
+             -Dsitearch=/usr/lib/perl5/5.32/site_perl    \
+             -Dvendorlib=/usr/lib/perl5/5.32/vendor_perl \
              -Dvendorarch=/usr/lib/perl5/5.32/vendor_perl
 
 make -j$(nproc)
